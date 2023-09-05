823f2df9df214a5ae01acba991b2f773b64b9c51
macoto1995 <adosute07@gmail.com>
2023-09-05 15:04:37 +0900

    zod openapi

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,69 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/aws-lambda'
+import { OpenAPIHono, createRoute } from '@hono/zod-openapi'
+import { z } from '@hono/zod-openapi'
 
-const app = new Hono()
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .min(3)
+    .openapi({
+      param: {
+        name: 'id',
+        in: 'path',
+      },
+      example: '1212121',
+    }),
+})
 
-app.get('/', (c) => c.text('Hello Hono!'))
+const UserSchema = z
+  .object({
+    id: z.string().openapi({
+      example: '123',
+    }),
+    name: z.string().openapi({
+      example: 'John Doe',
+    }),
+    age: z.number().openapi({
+      example: 42,
+    }),
+  })
+  .openapi('User')
 
-export const handler = handle(app)
+
+const route = createRoute({
+  method: 'get',
+  path: '/users/{id}',
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema,
+        },
+      },
+      description: 'Retrieve the user',
+    },
+  },
+})
+
+
+const app = new OpenAPIHono()
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid('param')
+  return c.jsonT({
+    id,
+    age: 20,
+    name: 'Ultra-man',
+  })
+})
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  },
+})
